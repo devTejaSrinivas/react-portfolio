30ecb717e9b9d435051879da408711249e430cbb
dev_tej <112886635+devTejaSrinivas@users.noreply.github.com>
2024-12-16 16:12:34 +0530

    Made the navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import logo from "../assets/logomts.png";
+import { FaLinkedin } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <nav className="mb-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <img className="mx-2 w-10" src={logo} alt="Logo" />
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4">
+        <FaLinkedin />
+        <FaGithub />
+        <FaInstagram />
+      </div>
+
+      {console.log("Component rendering")}
+    </nav>
+  );
+};
+
+export default Navbar;
