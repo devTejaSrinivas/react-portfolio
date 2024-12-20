1e399613ad85e3f8e98b2dca9ceab9ed525aafe0
dev_tej <112886635+devTejaSrinivas@users.noreply.github.com>
2024-12-20 22:39:34 +0530

    Added some content to about section and added gaps in the paragraph

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,9 +2,18 @@ import React from "react";
 import aboutImg from "../assets/AboutPic.png";
 import { ABOUT_TEXT } from "../constants";
 import { motion } from "framer-motion";
+
+const renderParagraphs = (text) => {
+  return text.split("\n\n").map((paragraph, index) => (
+    <p key={index} className="my-4">
+      {paragraph}
+    </p>
+  ));
+};
+
 const About = () => {
   return (
-    <div className="borde-b border-neutral-900 pb-4">
+    <div className="border-b border-neutral-900 pb-4">
       <h2 className="my-20 text-center text-4xl">About Me</h2>
 
       <div className="flex flex-wrap">
@@ -30,7 +39,9 @@ const About = () => {
           className="w-full lg:w-1/2"
         >
           <div className="flex justify-center lg:justify-start">
-            <p className="my-2 max-w-xl py-6">{ABOUT_TEXT}</p>
+            <div className="my-2 max-w-xl py-6">
+              {renderParagraphs(ABOUT_TEXT)}
+            </div>
           </div>
         </motion.div>
       </div>
