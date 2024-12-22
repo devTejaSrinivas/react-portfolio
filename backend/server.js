d9cf36d23d4438c3a0d33daaf1169657b00b7130
dev_tej <112886635+devTejaSrinivas@users.noreply.github.com>
2024-12-22 23:33:36 +0530

    Added cors origin

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,11 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://tejasrinivas.netlify.app",
+  })
+);
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
